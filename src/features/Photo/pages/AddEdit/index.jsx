640c78b7cf8605448d27494b96d9e92d2a1f5f70
minhquanghaiquan <minhquanghaiquan@gmail.com>
2020-06-14 10:45:44 +0200

    firstcommit

---
+++ src/features/Photo/pages/AddEdit/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Banner from '../../../../components/Banner';
+import PhotoForm from '../../components/PhotoForm';
+import './styles.scss';
+
+AddEditPage.propTypes = {
+    
+};
+
+function AddEditPage(props) {
+    return (
+        <div className="photo-edit">
+          <Banner title="Pick your amazing photo 😎" />
+    
+          <div className="photo-edit__form">
+            <PhotoForm
+            
+            />
+          </div>
+        </div>
+      );
+}
+
+export default AddEditPage;
